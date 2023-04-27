65ad5bde7355fdf58ceaf74bce08b389bd5135e1
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-04-27 16:03:14 +0900

    [FEATURE] BottomTabs 아이콘

--- src/navigations/RootTabs.tsx
+++ src/navigations/RootTabs.tsx
@@ -1,12 +1,28 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Messages from "../screens/Messages";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const BottomTabs = createBottomTabNavigator();
 
 const RootTabs = () => {
   return (
-    <BottomTabs.Navigator>
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = "";
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Messages") {
+            iconName = focused
+              ? "chatbox-ellipses"
+              : "chatbox-ellipses-outline";
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <BottomTabs.Screen name="Home" component={Home} />
       <BottomTabs.Screen name="Messages" component={Messages} />
     </BottomTabs.Navigator>
